093b062867923e873ebeca95d4eff84c4e9dca2b
annamarie244 <148158043+annamarie244@users.noreply.github.com>
2023-12-04 19:00:01 -0500

    Update HCE.py

--- HCE.py
+++ HCE.py
@@ -4,7 +4,3 @@ import streamlit as st
 
 st.title ("Healthcare Expenditures by Service and State 2020")
 
-HealthcareE = st.file_uploader("upload file", type={"csv", "txt"})
-if HealthcareE is not None:
-    HealthcareE_df = pd.read_csv(HealthcareE)
-st.write(HealthcareE_df)
