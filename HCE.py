98cddb17a7bf475d154c7dcc3ff620e6a096700f
annamarie244 <148158043+annamarie244@users.noreply.github.com>
2023-12-04 19:18:32 -0500

    Update HCE.py

--- HCE.py
+++ HCE.py
@@ -4,5 +4,5 @@ import streamlit as st
 
 st.title ("Healthcare Expenditures by Service and State 2020")
 
-healthcaredata = pd.read_csv ('Healthcare Expenditures.csv')
+healthcaredata = pd.read_csv ('/Users/annahowell/Documents/programming_directory/dsba5122/Healthcare-Expenditures-Service-and-State-2020/Untitled/HCE.py')
 healthcaredata
